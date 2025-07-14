5ebde82b70107ce7f60e2cdd726788064cf25ff0
Marcos Reyes <marcos@reyesandfriends.cl>
2025-07-14 02:08:34 -0400

    feat: Remove Footer component and add host script to package.json

--- marcos-reyes_card/src/layouts/layoutApp.tsx
+++ marcos-reyes_card/src/layouts/layoutApp.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/navbar/navbar'
-import Footer from './components/footer/footer'
 import BackgroundImg from '../assets/img/background.jpg'
 
 function LayoutApp() {
@@ -27,10 +26,6 @@ function LayoutApp() {
           <Outlet />
         </div>
       </main>
-
-      <div className="sticky bottom-0 z-20">
-        <Footer />
-      </div>
     </div>
   )
 }
