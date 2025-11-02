5e65e12a6269a038f8bafad10dcff08aeda5fe26
Marcos Reyes <astronautmarkus@gmail.com>
2025-11-02 14:45:23 -0300

    fix: corrected all filenames for case sensitive support.

--- astronautmarkus/src/main.tsx
+++ astronautmarkus/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Router from './router/Router'
+import Router from './router/router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- astronautmarkus/src/router/router.tsx
+++ astronautmarkus/src/router/router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LayoutApp from '../layout/LayoutApp';
-import Home from '../pages/Home/Home';
+import Home from '../pages/home/home';
 
 const Router = () => {
     return (
