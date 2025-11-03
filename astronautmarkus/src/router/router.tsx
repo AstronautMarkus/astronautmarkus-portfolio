b73880c6f39a7db4a33c2e4898f8df47d7da4a77
Marcos Reyes <astronautmarkus@gmail.com>
2025-11-02 23:54:42 -0300

    feat: refactor routing to use AnimatedRoutes for improved page transitions

--- astronautmarkus/src/router/router.tsx
+++ astronautmarkus/src/router/router.tsx
@@ -6,25 +6,75 @@ import AboutMe from '../pages/about-me/about-me';
 import TechStackView from '../pages/tech-stack/tech-stack';
 import React from 'react';
 
-function ScrollToTop() {
-    const { pathname } = useLocation();
+import { AnimatePresence, motion } from 'framer-motion';
+
+function AnimatedRoutes() {
+    const location = useLocation();
+    const [pendingScroll, setPendingScroll] = React.useState(false);
+
     React.useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
-    return null;
+        if (pendingScroll) {
+            window.scrollTo(0, 0);
+            setPendingScroll(false);
+        }
+    }, [pendingScroll]);
+
+    return (
+        <AnimatePresence
+            mode="wait"
+            onExitComplete={() => setPendingScroll(true)}
+        >
+            <Routes location={location} key={location.pathname}>
+                <Route element={<LayoutApp />}>
+                    <Route
+                        path="/"
+                        element={
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.4 }}
+                            >
+                                <Home />
+                            </motion.div>
+                        }
+                    />
+                    <Route
+                        path="/about-me"
+                        element={
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.4 }}
+                            >
+                                <AboutMe />
+                            </motion.div>
+                        }
+                    />
+                    <Route
+                        path="/tech-stack"
+                        element={
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.4 }}
+                            >
+                                <TechStackView />
+                            </motion.div>
+                        }
+                    />
+                </Route>
+            </Routes>
+        </AnimatePresence>
+    );
 }
 
 const Router = () => {
     return (
         <BrowserRouter>
-            <ScrollToTop />
-            <Routes>
-                <Route element={<LayoutApp />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about-me" element={<AboutMe />} />
-                    <Route path="/tech-stack" element={<TechStackView />} />
-                </Route>
-            </Routes>
+            <AnimatedRoutes />
         </BrowserRouter>
     );
 };
