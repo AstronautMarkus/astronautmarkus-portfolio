4d20480018c84f86e0f447a3d1eaea934edaff57
Marcos Reyes <astronautmarkus@gmail.com>
2025-11-03 00:11:12 -0300

    refactor: add Projects page and update routing to include it and deleted animations.

--- astronautmarkus/src/router/router.tsx
+++ astronautmarkus/src/router/router.tsx
@@ -1,48 +1,30 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import LayoutApp from '../layout/LayoutApp';
+import React from 'react';
 
 import Home from '../pages/home/home';
 import AboutMe from '../pages/about-me/about-me';
 import TechStackView from '../pages/tech-stack/tech-stack';
 import Contact from '../pages/contact/contact';
-
-import React from 'react';
-
-import { AnimatePresence, motion } from 'framer-motion';
+import Projects from '../pages/projects/projects';
 
 function AnimatedRoutes() {
     const location = useLocation();
-    const [pendingScroll, setPendingScroll] = React.useState(false);
 
     React.useEffect(() => {
-        if (pendingScroll) {
-            window.scrollTo(0, 0);
-            setPendingScroll(false);
-        }
-    }, [pendingScroll]);
+        window.scrollTo(0, 0);
+    }, [location]);
 
     return (
-        <AnimatePresence
-            mode="wait"
-            onExitComplete={() => setPendingScroll(true)}
-        >
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.4 }}
-                key={location.pathname}
-            >
-                <Routes location={location}>
-                    <Route element={<LayoutApp />}>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about-me" element={<AboutMe />} />
-                        <Route path="/tech-stack" element={<TechStackView />} />
-                        <Route path="/contact" element={<Contact />} />
-                    </Route>
-                </Routes>
-            </motion.div>
-        </AnimatePresence>
+        <Routes location={location}>
+            <Route element={<LayoutApp />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/about-me" element={<AboutMe />} />
+                <Route path="/tech-stack" element={<TechStackView />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/projects" element={<Projects />} />
+            </Route>
+        </Routes>
     );
 }
 
