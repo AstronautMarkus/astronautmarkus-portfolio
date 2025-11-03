c752d47ed63c95926bd5fa4c596cc70c03fdb109
Marcos Reyes <astronautmarkus@gmail.com>
2025-11-02 23:52:39 -0300

    feat: add ScrollToTop component for improved navigation experience

--- astronautmarkus/src/router/router.tsx
+++ astronautmarkus/src/router/router.tsx
@@ -1,13 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import LayoutApp from '../layout/LayoutApp';
 
 import Home from '../pages/home/home';
 import AboutMe from '../pages/about-me/about-me';
 import TechStackView from '../pages/tech-stack/tech-stack';
+import React from 'react';
+
+function ScrollToTop() {
+    const { pathname } = useLocation();
+    React.useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+    return null;
+}
 
 const Router = () => {
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Routes>
                 <Route element={<LayoutApp />}>
                     <Route path="/" element={<Home />} />
