712e03181b661a8727cd0f2f76d1fe3182b7ad22
Marcos Reyes <astronautmarkus@gmail.com>
2025-12-27 00:40:53 -0300

    feat: added i18n translations on Tech Stack page.

--- astronautmarkus/src/pages/tech-stack/tech-stack.tsx
+++ astronautmarkus/src/pages/tech-stack/tech-stack.tsx
@@ -1,8 +1,12 @@
 import TechStack from "./components/TechStack";
 import { motion } from "framer-motion";
 import SeoHelmet from "../../utils/SeoHelmet";
+import { useI18n } from "../../context/i18n";
 
 function TechStackView() {
+
+    const { t } = useI18n();
+
     return (
         <>
             <SeoHelmet
@@ -18,9 +22,9 @@ function TechStackView() {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.7, delay: 0.2 }}
                 >
-                    <h2 className="text-3xl lg:text-4xl font-bold text-rose-700 mb-4">My Tech Stack</h2>
+                    <h2 className="text-3xl lg:text-4xl font-bold text-rose-700 mb-4">{t("tech-stack.title")}</h2>
                     <p className="text-lg text-gray-600 max-w-3xl mx-auto leading-relaxed">
-                        Throughout my journey as a developer, I've had the opportunity to work with a <strong>wide variety of technologies and tools</strong>. Many might say <em>"you have quite the attention deficit"</em> for learning so many things, but I say, well, there's nothing wrong with adapting to different stacks beyond your own. <span className="text-rose-700 font-medium">Of course, I have my favorites</span>. Below you'll find a summary of the technologies I feel most comfortable working with and use most frequently in my projects.
+                        {t("tech-stack.description")}
                     </p>
                 </motion.div>
 
